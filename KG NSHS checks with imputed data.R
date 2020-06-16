24f7a8b3a4dfab0024231c0bf65d8542c59ae8c0
mollypak <50149666+mollypak@users.noreply.github.com>
2020-06-16 15:53:40 -0400

    Create KG NSHS checks with imputed data.R

---
+++ KG NSHS checks with imputed data.R
@@ -0,0 +1,30 @@
+
+
+propschecks_mi<-function(data, design,variab, outcome){
+  uni<-sort(unique(data[,variab]))
+  for(i in 1:length(uni)){
+    print.noquote(paste(variab, "=",uni[i]))
+    print.noquote(paste("Denominators:",table(data[,variab])[i]))
+    if(table(data[,variab])[i]<30) print.noquote(paste('demoninator less than 30 for',variab,"=",uni[i],"!SUPRESS!"))
+      print(paste(as.numeric(MIcombine(with(subset(design,get(variab)==uni[i]), exp=svymean(~iggpos, na.rm=TRUE)))[1]),
+                                   " (",kg(~iggpos,subset(design,get(variab)==uni[i]))[1],", ",
+                                   kg(~iggpos,subset(design,get(variab)==uni[i]))[2],")", sep = ""))
+         
+
+      
+    val<-MIcombine(with(subset(design,get(variab)==uni[i]), exp=svymean(~iggpos, na.rm=TRUE)))$coefficients[1]
+    upp<- kg(~iggpos,subset(design,get(variab)==uni[i]))[2]
+    lowe<-kg(~iggpos,subset(design,get(variab)==uni[i]))[1]
+    wid<-upp-lowe
+    if(wid>0.30) print.noquote(paste('absolute CI width >0.30 for',variab,"=",uni[i],"!SUPRESS!,absolute CI width=",wid))
+    if((wid/val)>1.30) print.noquote(paste('relative CI width >1.30 for',variab,"=",uni[i],"!SUPRESS!,absolute CI width=",wid/val))
+    
+    
+  }
+  
+}
+
+
+
+
+
