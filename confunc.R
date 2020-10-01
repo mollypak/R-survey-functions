8be9ce74357b989defae976b5eb109e673d98a7f
Molly Petersen <50149666+mollypak@users.noreply.github.com>
2020-10-01 18:59:59 -0400

    Add files via upload
    contrasts for predictive margins using multiply imputed survey data

---
+++ confunc.R
@@ -0,0 +1,30 @@
+confunc<-function(contrasts,stat){
+  
+  exprlist <- list(contrast = contrasts)
+  datalist<-as.list(stat) 
+  varmat<-as.matrix(attr(stat,"var"))
+  
+  
+  dexprlist <- lapply(exprlist, function(expr) deriv(expr, 
+                                                     
+                                                     names(datalist))[[1]])
+  
+  values <- lapply(dexprlist, function(dexpr) eval(do.call(substitute, 
+                                                           
+                                                           list(dexpr, datalist))))
+  jac <- do.call(rbind, lapply(values, function(value) attr(value, 
+                                                            
+                                                            "gradient")))
+  
+  var <- jac %*% varmat %*% t(jac)
+  
+  values <- do.call(c, values)
+  
+  dimnames(var) <- list(names(values), names(values))
+  
+  attr(values, "var") <- var
+  
+  
+  
+  values
+}
